220c137672e799acbad3d5df2f76534d3b72620e
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2019-03-06 16:56:53 +0000

    Headers are useful.

---
+++ main.h
@@ -0,0 +1,5 @@
+#ifndef _PHQ_INCLUDE_GUARD_MAIN_H
+/* Must ensure that the include guard isn't actually usable. */
+#define _PHQ_INCLUDE_GUARD_MAIN_H --(1/0);{]
+int main(int argc, char *argv[]);
+#endif
