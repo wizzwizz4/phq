8ddc8d6d20fd100572c864311ed527b215005c60
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2019-03-06 16:37:14 +0000

    Highest quality argument parsing, makefile and .gitignore!
    What could possibly be better?

---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    /* highest quality argument parsing */
+    for (int argi = 0; argi < argc; argi += 1) {
+        switch (argv[argi][0]) {
+            case '-':
+                switch (argv[argi][1]) {
+                    case '-':
+                        switch (argv[argi][2]) {
+                            case 'h':
+                                switch (argv[argi][3]) {
+                                    case 'e':
+                                        switch (argv[argi][4]) {
+                                            case 'l':
+                                                switch (argv[argi][5]) {
+                                                    case 'p':
+                                                        switch (argv[argi]
+                                                                [6]) {
+                                                            case '\0':
+                                                                goto opt_help;
+                                                        }
+                                                        break;
+                                                }
+                                                break;
+                                        }
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                    case '?':
+                        switch (argv[argi][2]) {
+                            case '\0':
+                            opt_help:
+                                printf("useful help\n");
+                                break;
+                        }
+                }
+                break;
+        }
+    }
+}
