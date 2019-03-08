2c97bba38b617f2b9e14227f2d59a9363d84bb01
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2019-03-08 15:37:15 +0000

    Beautiful compiler.

--- src/compiler.c
+++ src/compiler.c
@@ -1,8 +1,59 @@
+#include <stdlib.h>
 #include <stddef.h>
 #include "opcodes.h"
 
+char * compile_compile(const char **source,
+                       char *compiled, size_t *offset);
+
 char * compile(const char *source) {
-    size_t current_size = 0;
-    char *compiled = NULL;
-    return compiled;
+    size_t offset = 0;
+    return compile_compile(&source, NULL, &offset);
+}
+
+char * compile_compile(const char **source,
+                       char *compiled, size_t *offset) {
+    /* deal with NULL */
+    compiled = realloc(compiled - *offset, *offset);
+
+    do {
+        switch (*source[0]) {
+            case 'f':
+                switch (*source[1]) {
+                    case 'u':
+                        switch (*source[2]) {
+                            case 'n':
+                                switch (*source[3]) {
+                                    case 'c':
+                                        switch (*source[4]) {
+                                            case 't':
+                                                switch (*source[5]) {
+                                                    case 'i':
+                                                        switch (*source[6]) {
+                                                            case 'o':
+switch (*source[7]) {
+    case 'n':
+        *source += 7;
+        compiled = realloc(compiled - *offset, ++*offset) + *offset;
+        *compiled = FUNCTION_BLOCK;
+        compiled = compile_compile(source, compiled, offset);
+        compiled = realloc(compiled - *offset, ++*offset) + *offset;
+        *compiled = END_BLOCK;
+        break;
+}
+break;
+                                                        }
+                                                        break;
+                                                }
+                                                break;
+                                        }
+                                        break;
+                                }
+                                break;
+                        }
+                        break;
+                }
+                break;
+        }
+    } while (*(++*source) && **source != '}');
+    return compiled - *offset;
 }
--- src/opcodes.h
+++ src/opcodes.h
@@ -2,6 +2,8 @@
 #define _PHQ_INCLUDE_GUARD_OPCODES_H ][//e''"
 enum opcode {
     STACK_POP,
-    STACK_PUSH
+    STACK_PUSH,
+    FUNCTION_BLOCK,
+    END_BLOCK
 };
 #endif
