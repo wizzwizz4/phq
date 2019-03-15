c578f66fdc2405e9b460fd029b9daa3d49d3747f
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2019-03-15 08:48:45 +0000

    Added compile_varname.
    Also taught compiler to catch common errors.

--- src/compiler.c
+++ src/compiler.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 #include "opcodes.h"
 
-typedef char * (*compiler_func)(const char **restrict source,
+typedef char * (*compiler_func)(const char *restrict *const restrict source,
                                   char *restrict compiled,
                                   size_t *restrict offset);
 
@@ -15,15 +16,20 @@ typedef char * (*compiler_func)(const char **restrict source,
   compiled = (x)(source, compiled, offset) + *offset; \
 }while(0)
 
-char * compile_compile(const char **restrict source,
+char * compile_compile(const char *restrict *const restrict source,
                        char *restrict compiled, size_t *restrict offset);
+char * compile_varname_table(const char *restrict *const restrict source,
+                             char *restrict compiled,
+                             size_t *restrict offset);
 
-char * compile(const char *source) {
+char * compile_varname(const char *restrict *const restrict source);
+
+char * compile(const char *restrict source) {
     size_t offset = 0;
     return compile_compile(&source, NULL, &offset);
 }
 
-char * compile_compile(const char **restrict source,
+char * compile_compile(const char *restrict *const restrict source,
                        char *restrict compiled, size_t *restrict offset) {
     /* deal with NULL */
     compiled = (char *)realloc(compiled - *offset, *offset) + *offset;
@@ -48,6 +54,7 @@ switch (7[*source]) {
         *source += 7;
         _pwi_offset_inc(1);
         compiled[-1] = FUNCTION_BLOCK;
+        _vvi_call(compile_varname_table);
         _vvi_call(compile_compile);
         _pwi_offset_inc(1);
         compiled[-1] = END_BLOCK;
@@ -74,12 +81,15 @@ break;
 }
 
 
-char * compile_varname_table(const char **restrict source,
+char * compile_varname_table(const char *restrict *const restrict source,
                              char *restrict compiled,
                              size_t *restrict offset) {
+    _pwi_offset_inc(1);
+    compiled[-1] = VNMTAB;
     do {
         switch (0[*source]) {
             case '$':
+                compile_varname(source);
                 break;
             default:
                 goto _compiler_compile_varname_table_outer_loop_end;
@@ -87,6 +97,98 @@ char * compile_varname_table(const char **restrict source,
     } while (*++*source && **source != '}');
     _compiler_compile_varname_table_outer_loop_end:
     return compiled - *offset;
+}
+
+char * compile_varname(const char *restrict *const restrict source) {
+    if (**source != '$') {
+        return NULL;
+    }
+    *source += 1;
+
+    size_t buffer_size = 1;
+    size_t buffer_index = -1;
+    char *buffer = malloc(1);
+    do {
+        if (++buffer_index > buffer_size) {
+            buffer = realloc(buffer, buffer_size<<=1);
+        }
+        switch (**source) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            case 'F':
+            case 'G':
+            case 'H':
+            case 'I':
+            case 'J':
+            case 'K':
+            case 'L':
+            case 'M':
+            case 'N':
+            case 'O':
+            case 'P':
+            case 'Q':
+            case 'R':
+            case 'S':
+            case 'T':
+            case 'U':
+            case 'V':
+            case 'W':
+            case 'X':
+            case 'Y':
+            case 'Z':
+
+            case '_':
+
+            case 'a':
+            case 'b':
+            case 'c':
+            case 'd':
+            case 'e':
+            case 'f':
+            case 'g':
+            case 'h':
+            case 'i':
+            case 'j':
+            case 'k':
+            case 'l':
+            case 'm':
+            case 'n':
+            case 'o':
+            case 'p':
+            case 'q':
+            case 'r':
+            case 's':
+            case 't':
+            case 'u':
+            case 'v':
+            case 'w':
+            case 'x':
+            case 'y':
+            case 'z':
+                /* copy the thing */
+                buffer[buffer_index] = **source;
+                break;
+            default:
+                goto _compiler_compile_varname_outer_loop_end;
+        }
+    } while (*++*source && **source != '}');
+    _compiler_compile_varname_outer_loop_end:
+    *source -= 1;  /* do not consume next chaacter */
+    return buffer;
 }
 
 #undef _pwi_offset_inc
